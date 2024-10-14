fb92990bd01000be037731bb88ee510b421ff707
web-ghoul <mahmoudaboraya2021@gmail.com>
2024-10-14 04:27:11 -0700

    Set up New Portfolio Frontend

--- Backend/index.ts
+++ Backend/index.ts
@@ -1,7 +1,18 @@
+import cors from 'cors';
+import { config } from 'dotenv';
 import express, { Request, Response } from 'express';
+config();
 
 const app = express();
 const PORT = process.env.PORT || 4000;
+
+const corsOptions = {
+  origin: 'https://your-frontend-domain.com',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+};
+app.use(cors(corsOptions));
 
 // Define a route
 app.get('/', (req: Request, res: Response) => {
