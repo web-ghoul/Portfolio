e11f36a66d073801fdd34b9015f4ae0a87f96478
web-ghoul <mahmoudaboraya2021@gmail.com>
2024-12-04 00:23:06 -0800

    certificate is done in backend

--- Backend/index.ts
+++ Backend/index.ts
@@ -1,10 +1,11 @@
 import cors from 'cors';
 import { config } from 'dotenv';
 import express, { Request, Response } from 'express';
-import projectRouter from "./routes/projects.routes";
-import skillRouter from "./routes/skills.routes";
-import dbConnection from "./db/db";
-import errorHandler from "./utils/errorHandler.util";
+import dbConnection from './db/db';
+import certificateRouter from './routes/certificates.routes';
+import projectRouter from './routes/projects.routes';
+import skillRouter from './routes/skills.routes';
+import errorHandler from './utils/errorHandler.util';
 config();
 
 const app = express();
@@ -12,22 +13,23 @@ const PORT = process.env.PORT || 4000;
 
 const corsOptions = {
   origin: process.env.CLIENT_URL,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
 };
 app.use(cors(corsOptions));
 
 //Routes
-app.use("/api/projects", projectRouter);
-app.use("/api/skills", skillRouter);
+app.use('/api/projects', projectRouter);
+app.use('/api/certificates', certificateRouter);
+app.use('/api/skills', skillRouter);
 
 //Connect Dataase
 dbConnection();
 
 // Define a route
-app.get("/", (req: Request, res: Response) => {
-  res.send("Welcome to webGhoul Server");
+app.get('/', (req: Request, res: Response) => {
+  res.send('Welcome to webGhoul Server');
 });
 
 // Use the error handler middleware
--- Backend/middlewares/certificates.middleware.ts
+++ Backend/middlewares/certificates.middleware.ts
@@ -1,41 +1,44 @@
-import { NextFunction, Response } from "express";
-import { createSkillSchema, updateSkillSchema } from "../schemas/skill.schema";
-import AuthorizationRequestTypes from "../types/middlewares.types";
-import { handleValidationError } from "../utils/handleMiddleware.util";
-import Skill from "../models/skill.model";
-import CustomError from "../utils/customError.util";
+import { NextFunction, Response } from 'express';
+import Skill from '../models/skill.model';
+import {
+  createCertificateSchema,
+  updateCertificateSchema,
+} from '../schemas/certificate.schema';
+import AuthorizationRequestTypes from '../types/middlewares.types';
+import CustomError from '../utils/customError.util';
+import { handleValidationError } from '../utils/handleMiddleware.util';
 
-const createSkillValidation = (
+const createCertificateValidation = (
   req: AuthorizationRequestTypes,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
   const { file, body } = req;
-  const { error } = createSkillSchema.validate({
+  const { error } = createCertificateSchema.validate({
     ...body,
-    logo: file ? file.filename : "",
+    image: file ? file.filename : '',
   });
   handleValidationError(res, next, error);
 };
 
-const updateSkillValidation = (
+const updateCertificateValidation = (
   req: AuthorizationRequestTypes,
   res: Response,
-  next: NextFunction
+  next: NextFunction,
 ) => {
   const { file, body } = req;
-  const { error } = updateSkillSchema.validate({
+  const { error } = updateCertificateSchema.validate({
     ...body,
-    logo: file ? file.filename : "",
+    image: file ? file.filename : '',
   });
   handleValidationError(res, next, error);
 };
 
-const isSkillExist = (create?: boolean) => {
+const isCertificateExist = (create?: boolean) => {
   return async (
     req: AuthorizationRequestTypes,
     res: Response,
-    next: NextFunction
+    next: NextFunction,
   ) => {
     try {
       if (req.body) {
@@ -47,13 +50,13 @@ const isSkillExist = (create?: boolean) => {
         const skill = await Skill.findOne(query);
         if (skill) {
           return res.status(400).json({
-            message: "Skill name is duplicated",
+            message: 'Skill name is duplicated',
           });
         }
         return next();
       }
       res.status(500).json({
-        message: "No Data Sent",
+        message: 'No Data Sent',
       });
     } catch (error: any) {
       const err = new CustomError(error.message, 500);
@@ -62,4 +65,8 @@ const isSkillExist = (create?: boolean) => {
   };
 };
 
-export { createSkillValidation, updateSkillValidation, isSkillExist };
+export {
+  createCertificateValidation,
+  isCertificateExist,
+  updateCertificateValidation,
+};
